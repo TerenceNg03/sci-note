91836faf2196512855f5470d66bceab61eb4432a
TerenceNg03 <stoicism03@gmail.com>
2023-10-09 13:00:42 +0200

    Pack app with electron

--- app/Main.hs
+++ app/Main.hs
@@ -3,16 +3,28 @@
 module Main (main) where
 
 import Network.Wai.Middleware.RequestLogger (logStdout)
+import System.Environment (getArgs)
 import System.FilePath ((</>))
 import Web.Scotty (captureParam, file, get, middleware, regex, scotty, text)
 
 main :: IO ()
-main = scotty 3000 $ do
+main = do
+    args <- getArgs
+    let base = case length args of
+            x
+                | x >= 1 -> head args
+                | otherwise -> "./ui/build"
+    print args
+    putStrLn $ "Static directory: " ++ base
+    launchServer base
+
+launchServer :: FilePath -> IO ()
+launchServer base = scotty 3000 $ do
     middleware logStdout
-    get "/" $ file "ui/build/index.html"
+    get "/" $ file $ base </> "index.html"
     get "/api/:name" $ do
         name <- captureParam "name"
         text name
     get (regex "^/.*") $ do
         path <- captureParam "0"
-        file $ "ui/build" </> drop 1 path
+        file $ base </> drop 1 path
