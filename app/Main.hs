955b0b1972e7775e2830e8b67606c35f2c2ff6df
TerenceNg03 <stoicism03@gmail.com>
2023-10-09 11:12:31 +0200

    Setup Scotty with React ui

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import Lib
+import Network.Wai.Middleware.RequestLogger (logStdout)
+import System.FilePath ((</>))
+import Web.Scotty (captureParam, file, get, middleware, regex, scotty, text)
 
 main :: IO ()
-main = someFunc
+main = scotty 3000 $ do
+    middleware logStdout
+    get "/" $ file "ui/build/index.html"
+    get "/api/:name" $ do
+        name <- captureParam "name"
+        text name
+    get (regex "^/.*") $ do
+        path <- captureParam "0"
+        file $ "ui/build" </> drop 1 path
